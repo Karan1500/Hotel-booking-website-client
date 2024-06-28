358a5968a7e84daffad027d2779fad1f520dd71c
Karan Jivanramjiwala <karanhj786@gmail.com>
2024-06-28 15:53:41 +0530

    css related changes

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -53,9 +53,9 @@ function Register() {
                     {success && (<Success message='You have registered successfully'/>)}
                     <div className="bs">
                         <h2>Register</h2>
-                        <input type='text' className='form-control' placeholder='name' value={name} onChange={(e) => setName(e.target.value)}/>
-                        <input type='email' className='form-control' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-                        <input type='password' className='form-control' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        <input type='text' className='form-control newlog' placeholder='name' value={name} onChange={(e) => setName(e.target.value)}/>
+                        <input type='email' className='form-control newlog' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        <input type='password' className='form-control newlog' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                         <input type='password' className='form-control' placeholder='confirm password' value={cpassword} onChange={(e) => setCpassword(e.target.value)}/>
                         <button className="btn btn-primary mt-3" onClick={register} style={{backgroundColor: 'black', border: 'none'}}>Register</button>
                     </div>
